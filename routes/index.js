5382c69055340f24f78606aa3b9d0ec5a28d2dd7
MinhAnh2221 <131346133+MinhAnh2221@users.noreply.github.com>
2023-05-17 14:44:17 +0700

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+{
+  var express = require('express');
+  var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  router.get('/', (req, res) => {
+    ren.render("index")
+  });
 
-module.exports = router;
+  router.get('/could', (req, res) => {
+    res.render("could")
+  });
+  router.get('/seagame', (req, res) => {
+    res.render("seagames")
+  });
+  module.export = router;
+}
